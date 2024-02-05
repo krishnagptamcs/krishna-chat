cae8a240681133ae765193f082617fb1bbbdaf36
Krishna Gupta <121439479+krishnagptamcs@users.noreply.github.com>
2024-02-05 23:35:06 +0530

    initialising the server

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const { chats } = require("./dummyData/data");
+
+const app = express();
+
+app.get("/", (req, res) => res.send("Apka swagat hai "));
+
+app.get("/api/chats", (req, res) => res.send(chats));
+
+app.get("/api/chats/:id", (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+app.listen(5000, console.log("Server started on PORT 5000"));
